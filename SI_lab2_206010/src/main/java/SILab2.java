2df3f2d48843efce6fdc81936a88ed784b9d1b1b
Jordan Ivanov <jordanivanov2001@yahoo.com>
2022-06-03 22:36:14 +0200

    Dodavanje na kodo

--- SI_lab2_206010/src/main/java/SILab2.java
+++ SI_lab2_206010/src/main/java/SILab2.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SILab2 {
-}
+
+    public static List<String> function(List<String> list) {
+        if (list.size() <= 0) {//1
+            throw new IllegalArgumentException("List length should be greater than 0");//2
+        }
+        int n = list.size();//3
+        int rootOfN = (int) Math.sqrt(n);//3
+        if (rootOfN * rootOfN  != n) {//3
+            throw new IllegalArgumentException("List length should be a perfect square");//4
+        }
+        List<String> numMines = new ArrayList<>();//5
+        for (int i = 0; i < n; i++) {//6.1 ;6.2; 6.3
+            if (!list.get(i).equals("#")) {//7
+                int num = 0;//8
+                if ( (i % rootOfN != 0 && list.get(i - 1).equals("#")) || (i % rootOfN != rootOfN - 1 && list.get(i + 1).equals("#")) ) {//9
+                    if ( (i % rootOfN != 0 && list.get(i - 1).equals("#")) && (i % rootOfN != rootOfN - 1 && list.get(i + 1).equals("#")) ){//10
+                        num += 2;//11
+                    }
+                    else {
+                        num  += 1;//12
+                    }
+                }
+                if (i - rootOfN >= 0 && list.get(i - rootOfN).equals("#")){//13
+                    num++;//14
+                }
+                if (i + rootOfN < n && list.get(i + rootOfN).equals("#")){//15
+                    num++;//16
+                }
+                numMines.add(String.valueOf(num));//17
+            }
+            else {
+                numMines.add(list.get(i));//18
+            }
+        }
+        return numMines;//19
+    }
+}//20 - kraj na programata
--- SI_lab2_206010/src/test/java/SILab2Test.java
+++ SI_lab2_206010/src/test/java/SILab2Test.java
@@ -1,2 +1,42 @@
-public class SILab2Test {
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+    private List<String> createList(String... elems) {
+        return new ArrayList<>(Arrays.asList(elems));
+    }
+
+    @Test
+    void everyStatementandTest(){
+        RuntimeException ex;
+
+        ex = assertThrows(RuntimeException.class, ()->SILab2.function(createList()));
+        assertTrue(ex.getMessage().contains("List length should be greater than 0"));
+
+        ex= assertThrows(RuntimeException.class, ()-> SILab2.function(createList("0", "#", "0")));
+        assertTrue(ex.getMessage().contains("List length should be a perfect square"));
+
+        assertEquals(createList("1","0","1","#","3","#","2","#","2"), SILab2.function(createList("0","0","0","#","0","#","0","#","0")));
+
+    }
+    @Test
+    void everyBranchTest(){
+        RuntimeException ex;
+
+        ex = assertThrows(RuntimeException.class, ()->SILab2.function(createList()));
+        assertTrue(ex.getMessage().contains("List length should be greater than 0"));
+
+        ex= assertThrows(RuntimeException.class, ()-> SILab2.function(createList("0", "#", "0")));
+        assertTrue(ex.getMessage().contains("List length should be a perfect square"));
+
+        assertEquals(createList("1","0","1","#","3","#","2","#","2"), SILab2.function(createList("0","0","0","#","0","#","0","#","0")));
+
+    }
+
+
 }
